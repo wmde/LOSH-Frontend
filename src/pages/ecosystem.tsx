0d2110cac23f712959efd96146662f64dec891f0
M01M01 <MoisAddress@gmail.com>
2021-07-07 22:09:27 -0400

    feat: create ecosystem page

--- src/pages/ecosystem.tsx
+++ src/pages/ecosystem.tsx
@@ -1,17 +1,79 @@
-/* eslint-disable no-unused-vars */
 import * as React from "react";
-import { Link } from "gatsby";
-
 import Layout from "../components/layout";
-import SEO from "../components/seo";
+import { Typography, Row, Col, Card } from "antd";
+
+const gridStyle = {
+	width: "50%",
+	textAlign: "center"
+};
+
+const openNextCards = [
+	{
+		cardTitle: "Wikimedia Deutschland",
+		cardContent: "Lorem Ipsum dolor sit"
+	},
+	{
+		cardTitle: "Fraunhofer",
+		cardContent: "Lorem Ipsum dolor sit amet lorem"
+	},
+	{
+		cardTitle: "Wikifactory",
+		cardContent: "Lorem Ipsum dolor sit"
+	}
+];
+
+const openHardwareCards = [
+	{
+		cardTitle: "OSHWA",
+		cardContent: "Lorem Ipsum dolor sit"
+	},
+	{
+		cardTitle: "HardwareX",
+		cardContent: "Journal for OSH news"
+	},
+	{
+		cardTitle: "HardwareX",
+		cardContent: "Journal for OSH news"
+	},
+	{
+		cardTitle: "HardwareX",
+		cardContent: "Journal for OSH news"
+	}
+];
 
 const EcosystemPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+	<Layout>
+		<Typography.Title>Ecosystem</Typography.Title>
+		<Typography.Text>Lorem Ipsum dolor sit amet</Typography.Text>
+		<Row>
+			<Col>
+				<Card title="OPEN!NEXT Project">
+					{openNextCards.map(({ cardTitle, cardContent }) => {
+						return (
+							<Card.Grid hoverable={false} style={gridStyle}>
+								<Card type="inner" title={cardTitle}>
+									{cardContent}
+								</Card>
+							</Card.Grid>
+						);
+					})}
+				</Card>
+			</Col>
+			<Col>
+				<Card title="Open Hardware Ecosystem">
+					{openHardwareCards.map(({ cardTitle, cardContent }) => {
+						return (
+							<Card.Grid hoverable={false} style={gridStyle}>
+								<Card type="inner" title={cardTitle}>
+									{cardContent}
+								</Card>
+							</Card.Grid>
+						);
+					})}
+				</Card>
+			</Col>
+		</Row>
+	</Layout>
 );
 
 export default EcosystemPage;
