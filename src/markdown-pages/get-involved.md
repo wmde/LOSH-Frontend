8a65fd6674b11f275286ea43280733981f4962a2
Adam Hammad <mail@adamham.dev>
2021-07-19 17:24:51 +0200

    feature: convert static pages to markdown

---
+++ src/markdown-pages/get-involved.md
@@ -0,0 +1,22 @@
+---
+slug: "/get-involved"
+title: "Get Involved"
+---
+
+We will guide you in a step-by-step guide through the process of submitting OSH Data. Please consult ...
+
+## For Makers
+
+We will guide you in a step-by-step guide through the process of submitting OSH Data. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat
+
+[Wikimedia Deutschland](https://www.wikimedia.de/)
+
+[Wikimedia Deutschland](https://www.wikimedia.de/)
+
+## For Developers
+
+We will guide you in a step-by-step guide through the process of submitting OSH Data. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat
+
+[Wikimedia Deutschland](https://www.wikimedia.de/)
+
+[Wikimedia Deutschland](https://www.wikimedia.de/)
---
+++ src/templates/markdown-page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { graphql } from "gatsby";
+import rehypeReact from "rehype-react";
+import unified from "unified";
+import { Typography } from "antd";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const processor = unified().use(rehypeReact, {
+	createElement: React.createElement,
+	components: {
+	  h1: (props: any) => <Typography.Title level={1} {...props} />,
+	  h2: (props: any) => <Typography.Title level={2} {...props} />,
+	  h3: (props: any) => <Typography.Title level={3} {...props} />,
+	  h4: (props: any) => <Typography.Title level={4} {...props} />,
+	  h5: (props: any) => <Typography.Title level={5} {...props} />,
+	},
+  })
+  export const renderAst = (ast: any): JSX.Element => {
+	return (processor.stringify(ast) as unknown) as JSX.Element
+  }
+export default function Template({
+	//@ts-ignore
+	data // this prop will be injected by the GraphQL query below.
+}) {
+	const { markdownRemark } = data; // data.markdownRemark holds your post data
+	const { frontmatter, htmlAst } = markdownRemark;
+
+	return (
+		<Layout>
+			<SEO title={frontmatter.title} />
+			<Typography.Title level={1}>{frontmatter.title}</Typography.Title>
+			{renderAst(htmlAst)}
+		</Layout>
+	);
+}
+export const pageQuery = graphql`
+	query($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			htmlAst
+			frontmatter {
+				slug
+				title
+			}
+		}
+	}
+`;
