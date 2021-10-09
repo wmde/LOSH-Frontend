7700c10319a31b7d234e3cebaf2ebe551f827e88
M01M01 <MoisAddress@gmail.com>
2021-10-09 03:34:23 -0400

    chore: updated stub data

--- cypress/e2e/detail-view.spec.ts
+++ cypress/e2e/detail-view.spec.ts
@@ -4,14 +4,17 @@ describe("detail view page tests", () => {
 	beforeEach("match query and stub response", () => {
 		cy.intercept(
 			"GET",
-			"https://losh.ose-germany.de/w/api.php?action=wbgetentities&ids=Q513&format=json&origin=*",
+			"https://losh.ose-germany.de/w/api.php?action=wbgetentities&ids=Q438&format=json&origin=*",
 			{ fixture: "getEntityResponse" }
 		).as("getEntity");
-		cy.visit("/detail/Q513");
+		cy.visit("/detail/Q438");
 		cy.wait("@getEntity");
 	});
 
 	it("displays download button", () => {
-		cy.get(".anticon-download").should("be.visible");
+		cy.get(".anticon-download")
+			.should("be.visible")
+			.parents("a")
+			.should("have.attr", "href");
 	});
 });
