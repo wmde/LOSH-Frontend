6ec64e182b1283898706d565bca611a40a70eed2
M01M01 <MoisAddress@gmail.com>
2021-10-09 03:36:12 -0400

    feat: pagination and detail link test

--- cypress/e2e/explore-data.spec.ts
+++ cypress/e2e/explore-data.spec.ts
@@ -17,7 +17,7 @@ describe("explore data page tests", () => {
 			.should("be.visible");
 	});
 
-	it("table displays search results", () => {
+	it("displays search results in data table", () => {
 		cy.intercept(
 			"GET",
 			"https://losh.ose-germany.de/w/api.php?action=query&list=search&srsearch=robi+haswbstatement%3AP1426%3Dhttps%3A%2F%2Fgithub.com%2FOPEN-NEXT%2FOKH-LOSH%2Fraw%2Fmaster%2FOKH-LOSH.ttl%23Module&format=json&srnamespace=120&srlimit=10&sroffset=0&origin=*",
@@ -36,5 +36,43 @@ describe("explore data page tests", () => {
 			.find("tr")
 			.should("have.length", 5)
 			.should("be.visible");
+	});
+
+	it("pages through table data", () => {
+		cy.get(".ant-pagination-item-active")
+			.as("initialPage")
+			.next()
+			.invoke("attr", "title")
+			.then((title) => {
+				const nameColumnOrig = [];
+				cy.get(".ant-table-tbody>tr").each((tr) => {
+					const name = tr.attr("data-row-key").toString();
+					nameColumnOrig.push(name);
+				});
+
+				cy.get("@initialPage").next().click();
+				cy.location("href").should("contain", "page=" + title);
+				cy.wait(1000);
+
+				const nameColumnNext = [];
+				cy.get(".ant-table-tbody>tr").each((tr) => {
+					nameColumnNext.push(tr.attr("data-row-key").toString());
+				});
+
+				cy.then(() => {
+					expect(nameColumnOrig).to.not.deep.equal(nameColumnNext);
+				});
+			});
+	});
+
+	it("accessess table entry's detail page", () => {
+		cy.get(".ant-table-tbody>tr>td>a")
+			.first()
+			.as("detailLink")
+			.invoke("attr", "href")
+			.then((url) => {
+				cy.get("@detailLink").click();
+				cy.location("href").should("contain", url);
+			});
 	});
 });
