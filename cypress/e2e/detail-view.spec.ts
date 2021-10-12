34b66d6433ba8a53b895c6c856ae041d91ed6c5c
M01M01 <MoisAddress@gmail.com>
2021-10-12 00:07:51 -0400

    feat: detail view e2e

--- cypress/e2e/detail-view.spec.ts
+++ cypress/e2e/detail-view.spec.ts
@@ -5,16 +5,31 @@ describe("detail view page tests", () => {
 		cy.intercept(
 			"GET",
 			"https://losh.ose-germany.de/w/api.php?action=wbgetentities&ids=Q438&format=json&origin=*",
-			{ fixture: "getEntityResponse" }
+			{ fixture: "detailViewGetResponse.json" }
 		).as("getEntity");
 		cy.visit("/detail/Q438");
 		cy.wait("@getEntity");
 	});
 
-	it("displays download button", () => {
-		cy.get(".anticon-download")
+	it("has download button", () => {
+		cy.get("#downloadBtn")
 			.should("be.visible")
 			.parents("a")
 			.should("have.attr", "href");
+	});
+	it("has external repo link", () => {
+		cy.get("#repoBtn")
+			.should("be.visible")
+			.parents("a")
+			.should("have.attr", "href");
+	});
+
+	it("has back button", () => {
+		cy.get("#back")
+			.should("be.visible")
+			.click()
+			.wait(2000)
+			.location("pathname")
+			.should("eq", "/");
 	});
 });
