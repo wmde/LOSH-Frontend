9e599cb2d8664f51ac90d55df0f36d095f5ae843
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-01-28 16:00:54 +0100

    Make search clearable
    Fixes #88

--- packages/client/src/components/search-box.tsx
+++ packages/client/src/components/search-box.tsx
@@ -21,6 +21,7 @@ export const SearchBox: React.FC = () => {
 			<Input.Search
 				id="search"
 				placeholder="input search text"
+				allowClear={true}
 				onSearch={handleSearchChange}
 				onChange={(e) => setCurrentValue(e.target.value)}
 				value={currentValue}
